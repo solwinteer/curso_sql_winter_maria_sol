7c979842ea4991d5171abf8dce222a9afbd92858
solwinteer <156627225+solwinteer@users.noreply.github.com>
2024-09-29 13:37:03 -0300

    actualizado trigger

--- Triggers/tr_AfterUpdateTelefonoPaciente.sql
+++ Triggers/tr_AfterUpdateTelefonoPaciente.sql
@@ -1,18 +1,14 @@
--- Crear el Trigger para registrar cambios en el teléfono de los pacientes
+DELIMITER $$
+
 CREATE TRIGGER tr_AfterUpdateTelefonoPaciente
-ON Pacientes
-AFTER UPDATE
-AS
+AFTER UPDATE ON Pacientes
+FOR EACH ROW
 BEGIN
     -- Solo registrar cambios si el campo telefono ha sido modificado
-    IF UPDATE(telefono)
-    BEGIN
+    IF OLD.telefono != NEW.telefono THEN
         INSERT INTO HistorialTelefonos (id_paciente, telefono_anterior, telefono_nuevo)
-        SELECT 
-            i.id, -- ID del paciente
-            d.telefono, -- Teléfono anterior
-            i.telefono -- Nuevo teléfono
-        FROM inserted i
-        INNER JOIN deleted d ON i.id = d.id; -- Unir registros actualizados con los anteriores
-    END
-END;
+        VALUES (NEW.id, OLD.telefono, NEW.telefono);
+    END IF;
+END$$
+
+DELIMITER ;
